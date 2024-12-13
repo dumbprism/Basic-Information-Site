4dd1f5b6263a7865d2686a4a48faef1c3012cd4a
Dumbprism <arhantk915@gmail.com>
2024-12-13 12:20:34 +0530

    basic information site

--- killbill.md
+++
@@ -1,3 +0,0 @@
-this is my prayer 
- 
- but before watching kill bill 2 you must be familiar with the story of kill bill 1
--- server2.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs')
-
-const rs = fs.createReadStream('killbill.md')
-
-rs.on('open',() => {
-    console.log('The file is open')
-})
-
-
