9858fcc1d5c77e5d8dbf1c693b10877627ff534f
Dumbprism <arhantk915@gmail.com>
2024-12-13 13:02:59 +0530

    updated the server logic that accepts the 404 page as well

--- server.js
+++ server.js
@@ -1,22 +1,36 @@
 const http = require('http')
-const url = requrire('url')
+const url = require('url')
 const fs = require('fs')
 
 http.createServer((req,res) => {
     const q = url.parse(req.url,true)
     const filename = '.' + q.pathname
+    
 
     fs.readFile(filename,(err,data) => {
 
        if(err){
-        res.writeHead(404,{'Content-Type' : 'text/html'})
-        return res.end('404.html')
+
+        fs.readFile('404.html',(err404,data404) => {
+
+            if(err404){
+                res.writeHead(404,{
+                    'Content-Type' : "text/html"
+                })
+                res.write('<h1> some unknown error occurred, please try again </h1>')
+            }
+
+            res.writeHead(404,{'Content-Type' : 'text/html'});
+            res.write(data404)
+            return res.end()
+
+        })
+
+       } 
+       else{
+        res.writeHead(200,{'Content-Type': 'text/html'});
+        res.write(data)
+        return res.end()
        }
-       
-       res.writeHead(200,{'Content-Type': 'text/html'});
-       res.write(data)
-       return res.end()
-       
     })
-    
 }).listen(8081)
